063c97e8206f85059c6f828f7061314648b81d84
jw397 <jw397@126.com>
2018-07-10 17:57:04 +0800

    feat: concat添加缓存

--- src/concat.js
+++ src/concat.js
@@ -4,20 +4,25 @@ const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 
 const util = require('./util');
+const Cache = require('cache-swap');
+const cache = require('gulp-cache');
 
-let devDir,distDir;
+let devDir, distDir, fileCache;
 
 let concatUtil = function (arr, name, cwd, dest) {
   cwd = cwd || devDir;
   dest = dest || distDir + '/js';
   var deffered = Q.defer();
   gulp.src(arr, {read: true, cwd: cwd})
-    .pipe(uglify({
+    .pipe(cache(uglify({
       compress: {
         drop_console: true, //删除console
         drop_debugger: true, //删除debugger
         typeofs: false //typeof xxx === "undefined"
       }
+    }), {
+      fileCache: fileCache,
+      name: 'concat'
     }))
     .pipe(concat(name))
     .pipe(gulp.dest(dest))
@@ -46,9 +51,21 @@ var concatFun = function (map, promises) {
   }
 };
 
-module.exports = function (fileList, _devDir, _distDir) {
+module.exports = function (fileList, _devDir, _distDir, _cacheDirName) {
   devDir = _devDir;
   distDir = _distDir;
+  cacheDirName = _cacheDirName || 'dcv-fl';
+  if (util.isWindows()) { //windows版本
+    fileCache = new Cache({
+      'cacheDirName': cacheDirName,
+      'tmpDir': 'C:\\Documents\\gulp-cache'
+    });
+  } else {
+    fileCache = new Cache({
+      'cacheDirName': cacheDirName,
+      'tmpDir': '../gulp-cache'
+    });
+  }
   var promises = [];
   concatFun(fileList.js, promises);
   promises.push(util.writeToFile(distDir + '/file_list.json', JSON.stringify(fileList, null, '\t')));
